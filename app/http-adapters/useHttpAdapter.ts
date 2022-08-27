e67d9a4e81303f0978121d6f371c2202d1800ccc
Eric Echemane <eechemane29@gmail.com>
2022-08-28 06:35:45 +0800

    remove try catch to catch error ahead of time if no body is given in a post request

--- app/http-adapters/useHttpAdapter.ts
+++ app/http-adapters/useHttpAdapter.ts
@@ -16,43 +16,42 @@ export default function useHttpAdapter<PayloadType>(adapter: HttpAdapter) {
         setError(null);
         setResponse(null);
         setData(null);
-        try {
-            if (adapter.method === 'POST' && !options?.payload) {
-                throw new Error('Payload is required for POST requests');
-            }
 
-            if (options?.params) {
-                adapter.parseUrlWith(options?.params);
-            }
-            let response: any;
-
-            if (adapter.method === 'GET') {
-                response = await fetch(adapter.url);
-            }
-            else {
-                response = await fetch(adapter.url, {
-                    method: adapter.method,
-                    body: JSON.stringify(options?.payload),
-                    headers: { 'Content-Type': 'application/json' }
-                });
-            }
-
-            setResponse(response);
-
-            if (response.ok) {
-                const data = await response.json();
-                setData(data.data);
-                return data;
-            }
-            else {
-                const error = await response.json();
-                setError(error);
-            }
-        } catch (error: any) {
-            setError(error);
-        } finally {
-            setLoading(false);
+        if (adapter.method === 'POST' && !options?.payload) {
+            throw new Error('Payload is required for POST requests');
         }
+
+        if (options?.params) {
+            adapter.parseUrlWith(options?.params);
+        }
+
+        let response: any;
+
+        if (adapter.method === 'GET') {
+            response = await fetch(adapter.url);
+        }
+        else {
+            response = await fetch(adapter.url, {
+                method: adapter.method,
+                body: JSON.stringify(options?.payload),
+                headers: { 'Content-Type': 'application/json' }
+            });
+        }
+
+        setResponse(response);
+
+        if (response.ok) {
+            const data = await response.json();
+            setData(data.data);
+            return data;
+        }
+        else {
+            const error = await response.json();
+            setError(error);
+        }
+
+        setLoading(false);
+
     }, [adapter]);
 
     return {
